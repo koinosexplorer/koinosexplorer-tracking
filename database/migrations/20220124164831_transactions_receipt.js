84de72b50c05c884c645658b58cca5748d3a5b1f
eder lobo <ederandreslobo1998@gmail.com>
2022-03-08 17:01:29 -0500

    add table receipts

---
+++ database/migrations/20220124164831_transactions_receipt.js
@@ -0,0 +1,25 @@
+exports.up = function(knex) {
+  return knex.schema.createTable('transactions_receipts',function(table){
+    /**
+     * Data
+     */
+    table.string('id').primary().comment('random id for backend')
+    table.string('name').notNullable();
+    table.text('value').nullable();
+    
+    /**
+     * Relations
+     */
+     table.string('transaction_id').references('transaction_id').inTable('transactions').notNullable();
+
+    /**
+     * Default data
+     */
+    table.timestamp('created_at')
+    table.timestamp('updated_at')
+  })
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists('transactions_receipts')
+};
